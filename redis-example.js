a5332721dd4318ee4d6d13a5d01ff8c98a267f04
Luise Valentin <luise@quilt.io>
2017-07-22 17:46:57 -0700

    blueprint: Lint blueprints

--- redis-example.js
+++ redis-example.js
@@ -1,5 +1,5 @@
-const {createDeployment, Machine, githubKeys} = require("@quilt/quilt");
-const Redis = require("./redis.js");
+const {createDeployment, Machine, githubKeys} = require('@quilt/quilt');
+const Redis = require('./redis.js');
 
 const deployment = createDeployment();
 
@@ -7,11 +7,11 @@ const nWorker = 2;
 
 // Boot redis with 2 workers and 1 master. AUTH_PASSWORD is used to secure
 // the redis connection
-var rds = new Redis(nWorker, "AUTH_PASSWORD");
+let rds = new Redis(nWorker, 'AUTH_PASSWORD');
 
-var baseMachine = new Machine({
-    provider: "Amazon",
-    sshKeys: githubKeys("ejj"), // Replace with your GitHub username.
+let baseMachine = new Machine({
+    provider: 'Amazon',
+    sshKeys: githubKeys('ejj'), // Replace with your GitHub username.
 });
 
 deployment.deploy(baseMachine.asMaster());
--- redis.js
+++ redis.js
@@ -1,8 +1,13 @@
-const {Service, Container} = require("@quilt/quilt");
+const {Service, Container} = require('@quilt/quilt');
 
 const port = 6379;
-const image = "quilt/redis";
+const image = 'quilt/redis';
 
+/**
+ * Creates a replicated Redis database.
+ * @param {number} nWorker - The desired number of Redis replicas.
+ * @param {string} auth - The password for authenticating with Redis instances.
+ */
 function Redis(nWorker, auth) {
     this.master = createMaster(auth);
     this.workers = createWorkers(nWorker, auth, this.master);
@@ -11,31 +16,43 @@ function Redis(nWorker, auth) {
 
     this.deploy = function(deployment) {
         deployment.deploy([this.master, this.workers]);
-    }
+    };
 
     // Only masters can accept write requests, so for simplicity, allowFrom
     // only connects other services to the master.
     this.allowFrom = function(senderService, port) {
       this.master.allowFrom(senderService, port);
-    }
+    };
 }
 
+/**
+ * Creates a service with a master Redis instance.
+ * @param {string} auth - The password for autheticating with Redis.
+ * @return {Service} - The Redis master service.
+ */
 function createMaster(auth) {
-    return new Service("redis-ms", [
-        new Container(image, ["run"]).withEnv({
-            "ROLE": "master",
-            "AUTH": auth,
-        })
+    return new Service('redis-ms', [
+        new Container(image, ['run']).withEnv({
+            'ROLE': 'master',
+            'AUTH': auth,
+        }),
     ]);
 }
 
+/**
+ * Creates a service with replicated Redis workers.
+ * @param {number} n - The desired number of workers.
+ * @param {string} auth - The password for autheticating with Redis.
+ * @param {Service} master - The master Redis service.
+ * @return {Service} - The worker Redis service.
+ */
 function createWorkers(n, auth, master) {
-    var refWorker = new Container(image, ["run"]).withEnv({
-        "ROLE": "worker",
-        "AUTH": auth,
-        "MASTER": master.hostname(),
+    let refWorker = new Container(image, ['run']).withEnv({
+        'ROLE': 'worker',
+        'AUTH': auth,
+        'MASTER': master.hostname(),
     });
-    return new Service("redis-wk", refWorker.replicate(n));
+    return new Service('redis-wk', refWorker.replicate(n));
 }
 
 module.exports = Redis;
